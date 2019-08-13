61f2d7eb830f6c990f0432a904f2ab16f90036e6
Chris Heinrich <cpheinrich@gmail.com>
2019-08-13 08:05:57 -0700

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
 
+
+def required_packages():
+    PACKAGES = [
+        'torch==1.1.0',
+        'torchvision==0.2.2.post3'
+    ]
+    return PACKAGES
+
+
 setup(
     name='harmonic-networks',
-    version='0.0.1',
+    version='0.0.2',
     description='PyTorch reimplementation of harmonic networks',
     packages=['harmonic'],
+    install_requires=required_packages(),
     author='Michał Tyszkiewicz',
     author_email='michal.tyszkiewicz@gmail.com',
 )
